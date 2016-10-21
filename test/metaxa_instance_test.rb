87126304132b8937ecce64378da072ba789f7607
mckomo <mckomo@gmail.com>
2016-10-21 17:28:05 +0200

    New tests

---
+++ test/metaxa_instance_test.rb
@@ -0,0 +1,25 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'bundler/setup'
+require 'metaxa'
+require 'test/unit'
+require 'colorize'
+
+extend Test::Unit::Assertions
+
+class FooInstance
+  include Metaxa
+
+  def setup(number)
+    introduce :lucky_number, with_value: number
+  end
+end
+
+foo = FooInstance.new
+foo.setup(14)
+assert_equal foo.lucky_number, 14
+
+foo_without_setup = FooInstance.new
+assert_raises(NoMethodError) { foo_without_setup.lucky_number }
+
+puts 'Instance test completed'.colorize(:green)
